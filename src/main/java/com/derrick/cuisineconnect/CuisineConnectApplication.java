7dc3c959f15c6df20a2132d48f957cc67c35958b
Kachielite <40024356+Kachielite@users.noreply.github.com>
2024-02-18 19:40:24 +0300

    Database setup and initial setup

---
+++ src/main/java/com/derrick/cuisineconnect/CuisineConnectApplication.java
@@ -0,0 +1,13 @@
+package com.derrick.cuisineconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuisineConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuisineConnectApplication.class, args);
+    }
+
+}
